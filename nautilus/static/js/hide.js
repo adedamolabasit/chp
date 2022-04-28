6b62721e1e788baecfe26f35be1a0690ee4eddd3
adedamolabasit <53799680+adedamolabasit@users.noreply.github.com>
2022-04-29 00:29:37 +0100

    ---

--- nautilus/static/js/hide.js
+++ nautilus/static/js/hide.js
@@ -1,4 +1,39 @@
-const button = document.getElementById('hide')
-button.classList.add("show")
-const checkbox = document.querySelectorAll('acb')
-console.log(checkbox)
+let count = 0;
+let checkboxes = document.querySelectorAll(".acb"); //node list
+let parent = document.querySelectorAll(".parent"); //node list
+// change to array 
+let checkboxArray = Array.from(checkboxes); //array
+let parentArray = Array.from(parent); //array
+
+let heading = document.querySelector("#hide"); //heading element
+
+
+for (let i = 0; i < checkboxArray.length; i++) {
+  parentArray[i].addEventListener("click", () => {
+    if (checkboxArray[i].checked === false) {
+      checkboxArray[i].checked === true;
+      count++;
+      console.log(count)
+
+    }else {
+        checkboxArray[i].checked === false;
+        // count--;
+        if (count !== 0) {
+      count--;
+    }
+    }
+
+    // if (!checkboxArray[i].checked && count !== 0) {
+    //   count--;
+    // }
+    if (count == 2) {
+      heading.classList.add("show");
+    } else {
+      heading.classList.remove("show");
+    }
+  });
+}
+
+
+
+
