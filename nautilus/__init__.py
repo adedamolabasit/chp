0adb80ad4bf1491187266aa77377fed3973c106a
adedamolabasit <53799680+adedamolabasit@users.noreply.github.com>
2022-04-29 12:33:07 +0100

    ---

--- nautilus/__init__.py
+++ nautilus/__init__.py
@@ -8,6 +8,7 @@ from flask_mail import Mail
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wtzyegodvafvqe:5592a0206fd4d997420f01e6201a345567756caea4f66488cec2381671be061a@ec2-3-212-45-192.compute-1.amazonaws.com:5432/d2ap268nr1oibk'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yourusername:yourpassword@localhost:5432/yourdatabasename'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 app.config['SECRET_KEY']='shfrfkhhenvrdgdg6djjguigv@ft'
 db=SQLAlchemy(app)
